7cac1c5a1754b66c94a09675248b41768090d516
prith <prithvijeet@gmail.com>
2020-06-30 14:08:47 -0400

    updating

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include "mpmc.hpp"
 #include <vector>
 #include <mutex>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
 using std::mutex;
 using std::thread;
+using std::vector;
 
 MPMCQ<int> data;
 std::atomic<int> total_count;
 
-void read(int id) {
+
+void read(int id, vector<int> vec) {
     static thread_local int count;
     static thread_local int pos = 0;
     while(1) {
         if(int val = data.read()) {
+            vec.push_back(val);
             //cout << "thread " << id << " read idx: " << val << endl;
             ++count;
         }
@@ -41,9 +45,12 @@ int main() {
         //total_count += 1;
         //total_count -= 1;
     }
-    
-    thread t3(read,1);
-    thread t4(read,2);
+    vector<int> v1;
+    v1.reserve(num_elements);
+    vector<int> v2;
+    v2.reserve(num_elements);
+    thread t3(read,1,v1);
+    thread t4(read,2,v2);
     //thread t5(read);
     //thread t6(read);
 
@@ -63,5 +70,16 @@ int main() {
         cout << "DID NOT WRITE ALL " << num_elements << " elements!";
     }
     cout << "total read element count is " << total_count << endl;
+    std::sort(v1.begin(), v1.end());
+    std::sort(v2.begin(), v2.end());
+    
+    std::vector<int> v_intersection;
+ 
+    std::set_intersection(v1.begin(), v1.end(),
+                          v2.begin(), v2.end(),
+                          std::back_inserter(v_intersection));
+    cout << v_intersection.size() << endl;
+    for(int n : v_intersection)
+        std::cout << n << ' ';
 
 }
