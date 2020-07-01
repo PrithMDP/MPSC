738de4d5874a2040ae083c12133d6b2cc9ea46a0
prith <prithvijeet@gmail.com>
2020-06-30 23:06:19 -0400

    bug fix

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ void read(int id, vector<int>& vec) {
     static thread_local int count;
     static thread_local int pos = 0;
     while(1) {
-        if(int val = data.read()) {
+        if(int val = data.read(id)) {
             vec.push_back(val);
             //cout << "thread " << id << " read idx: " << val << endl;
             ++count;
@@ -31,34 +31,34 @@ void read(int id, vector<int>& vec) {
     total_count += count;
 }
 
-void write() {
-    while(data.write()) {}
+void write(int id) {
+    while(data.write(id)) {}
 }
 
 int main() {
     total_count.store(0);
-    thread t1(write);
-    //thread t2(write);
+    thread t1(write,0);
+    thread t2(write,1);
     
-    for(int i = 0; i < 1000000; ++i) {
-        //total_count += 1;
-        //total_count -= 1;
-    }
     vector<int> v1;
-    v1.reserve(num_elements);
     vector<int> v2;
+    vector<int> v4;
+    vector<int> v3;
+    v1.reserve(num_elements);
     v2.reserve(num_elements);
+    v3.reserve(num_elements);
+    v4.reserve(num_elements);
     thread t3(read,1,std::ref(v1));
     thread t4(read,2,std::ref(v2));
-    //thread t5(read);
-    //thread t6(read);
+    thread t5(read,2,std::ref(v3));
+    //thread t6(read,2,std::ref(v4));
 
     t1.join();
-    //t2.join();
+    t2.join();
 
     t3.join();
     t4.join();
-    //t5.join();
+    t5.join();
     //t6.join();
 
     int count = 0;
@@ -78,13 +78,14 @@ int main() {
                           v2.begin(), v2.end(),
                           std::back_inserter(v_intersection));
     assert(v_intersection.size() == 0);
+    /*
     cout << " Reader 1 indices: " << endl;
     for(auto &x : v1) cout << x << " ";
     cout << endl;
     cout << " Reader 2 indices: " << endl;
     for(auto& x: v2) cout << x << " ";
     cout << endl;
-
+    */
     assert((total_count == num_elements));
 
 }
